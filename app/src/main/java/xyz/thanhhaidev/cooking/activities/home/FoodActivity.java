462e55344e9c5a623b0be5f4095c4784fc5fa3d8
Thanh Hai Dev <hai2571998@gmail.com>
2018-06-01 16:01:11 +0700

    Add

--- app/src/main/java/xyz/thanhhaidev/cooking/activities/home/FoodActivity.java
+++ app/src/main/java/xyz/thanhhaidev/cooking/activities/home/FoodActivity.java
@@ -2,6 +2,7 @@ package xyz.thanhhaidev.cooking.activities.home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import xyz.thanhhaidev.cooking.BaseActivity;
@@ -9,10 +10,13 @@ import xyz.thanhhaidev.cooking.R;
 
 public class FoodActivity extends BaseActivity {
 
+    GridView gvMaterial;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+        gvMaterial = findViewById(R.id.gvMaterial);
 
         Intent intent = getIntent();
         String id = intent.getStringExtra("id");
