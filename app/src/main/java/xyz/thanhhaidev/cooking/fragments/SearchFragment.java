335d7b833f83cf44ecd03c01cb6f5693263bd440
Thanh Hai Dev <hai2571998@gmail.com>
2018-06-04 13:31:25 +0700

    Added Fab Fav

--- app/src/main/java/xyz/thanhhaidev/cooking/fragments/SearchFragment.java
+++ app/src/main/java/xyz/thanhhaidev/cooking/fragments/SearchFragment.java
@@ -47,7 +47,6 @@ public class SearchFragment extends Fragment {
     private static String TAG = "SearchFragment";
 
     public SearchFragment() {
-        // Required empty public constructor
     }
 
 
